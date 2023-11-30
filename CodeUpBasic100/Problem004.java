1ec5c7413e4da150d26f53448b973937736096ed
yoowonjun <twojunspace@naver.com>
2023-11-30 13:08:31 +0900

    4th Problem solution commit

--- CodeUpBasic100/Problem004.java
+++ CodeUpBasic100/Problem004.java
@@ -3,6 +3,6 @@ package CodeUpBasic100;
 public class Problem004 {
 
     public static void main(String[] args) {
-        System.out.println("\'Hello\'");
+        System.out.println("/'Hello/'");
     }
 }
