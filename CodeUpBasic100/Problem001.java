92ed4c1890314cdb0cd44497e087218966a337c0
yoowonjun <twojunspace@naver.com>
2023-11-30 13:10:50 +0900

    4th Problem solution commit

--- CodeUpBasic100/Problem001.java
+++ CodeUpBasic100/Problem001.java
@@ -1,3 +1,5 @@
+package CodeUpBasic100;
+
 public class Problem001 {
     public static void main(String[] args) {
         System.out.println("Hello");
--- CodeUpBasic100/Problem002.java
+++ CodeUpBasic100/Problem002.java
@@ -1,3 +1,5 @@
+package CodeUpBasic100;
+
 public class Problem002 {
     public static void main(String[] args) {
         System.out.println("Hello World");
