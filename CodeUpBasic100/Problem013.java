29e95972bbd591324c81f125798bc9e96e882afc
yoowonjun <twojunspace@naver.com>
2023-12-03 22:04:08 +0900

    14th Problem solution commit

---
+++ CodeUpBasic100/Problem013.java
@@ -0,0 +1,16 @@
+package CodeUpBasic100;
+
+import java.util.Scanner;
+
+public class Problem013 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.next();
+        String b = scanner.next();
+
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
+
---
+++ CodeUpBasic100/Problem014.java
@@ -0,0 +1,15 @@
+package CodeUpBasic100;
+
+import java.util.Scanner;
+
+public class Problem014 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double input = scanner.nextDouble();
+
+        for (int count = 1; count <= 3; count++) {
+            System.out.println(input);
+        }
+    }
+}
