3d7c87da2d63e1f28b75b6767be478f1007c34e7
yoowonjun <twojunspace@naver.com>
2023-12-05 23:21:12 +0900

    32th Problem solution commit

---
+++ CodeUpBasic100/Problem030.java
@@ -0,0 +1,15 @@
+package CodeUpBasic100;
+
+import java.util.Scanner;
+
+public class Problem030 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char c = scanner.nextLine()
+                .charAt(0);
+
+        int result = c;
+        System.out.println(result);
+    }
+}
---
+++ CodeUpBasic100/Problem031.java
@@ -0,0 +1,13 @@
+package CodeUpBasic100;
+
+import java.util.Scanner;
+
+public class Problem031 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        char result = (char) number;
+        System.out.println();
+    }
+}
