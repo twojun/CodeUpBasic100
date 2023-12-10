71597b38416c0b41794baa1409e0a8a8ef725068
yoowonjun <twojunspace@naver.com>
2023-12-11 00:01:12 +0900

    37th problem solving commit

--- CodeUpBasic100/Problem037.java
+++ CodeUpBasic100/Problem037.java
@@ -1,20 +1,25 @@
 package CodeUpBasic100;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.Scanner;
 
 public class Problem037 {
 
     public static void main(String[] args) throws IOException {
-            Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            int count = scanner.nextInt();
-            scanner.nextLine();
-            String str = scanner.nextLine();
+        int count = Integer.parseInt(br.readLine());
+        String str = br.readLine();
 
-            for (int i = 0; i < count; i++) {
-                System.out.printf(str);
-            }
+        for (int i = 0; i < count; i++) {
+            System.out.printf(str);
+        }
+
+
+
 
     }
 }
