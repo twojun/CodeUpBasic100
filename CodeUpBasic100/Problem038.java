a2de3e3f2ca6d8993a88a50da2d8d0ff237908db
yoowonjun <twojunspace@naver.com>
2023-12-11 23:22:16 +0900

    40th problem solving commit

--- CodeUpBasic100/Problem038.java
+++ CodeUpBasic100/Problem038.java
@@ -5,17 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Problem037 {
+public class Problem038 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-        int count = Integer.parseInt(br.readLine());
-        String str = br.readLine();
-
-        for (int i = 0; i < count; i++) {
-            System.out.printf(str);
-
-        }
+        // Math.pow의 반환값은 double형이다.
+        System.out.printf("%.0f", Math.pow(a, b));
     }
 }
--- CodeUpBasic100/Problem040.java
+++ CodeUpBasic100/Problem040.java
@@ -5,17 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Problem037 {
+public class Problem040 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int count = Integer.parseInt(br.readLine());
-        String str = br.readLine();
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < count; i++) {
-            System.out.printf(str);
+        System.out.println(a / b);
 
-        }
     }
 }
