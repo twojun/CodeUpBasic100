163efba363d04e4aacf3363dfa9918d5887c1c76
yoowonjun <twojunspace@naver.com>
2023-12-21 01:34:54 +0900

    53th problem solving commit

--- CodeUpBasic100/Problem052.java
+++ CodeUpBasic100/Problem052.java
@@ -3,7 +3,6 @@ package CodeUpBasic100;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Problem052 {
 
