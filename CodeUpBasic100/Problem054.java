64b2419474e52be51b5263f02266875dee0c004b
yoowonjun <twojunspace@naver.com>
2024-01-02 21:33:03 +0900

    54th problem solving commit

--- CodeUpBasic100/Problem054.java
+++ CodeUpBasic100/Problem054.java
@@ -3,18 +3,28 @@ package CodeUpBasic100;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Problem054 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int i = Integer.parseInt(st.nextToken());
-        int j = Integer.parseInt(st.nextToken());
-        if (i == 1 && j == 1) {
-            System.out.println("True");
-        } else {
-            System.out.println("False");
+        BigInteger sum  = new BigInteger("0");
+
+        for (int i = 0; i < 3; i++) {
+            int count = Integer.parseInt(br.readLine());
+            for (int j = 0; j < count; j++) {
+                BigInteger input  = new BigInteger(br.readLine());
+                sum = sum.add(input);
+            }
+
+            if (sum.compareTo(BigInteger.ZERO) == -1) {
+                System.out.println("-");
+            } else if (sum.compareTo(BigInteger.ZERO) == 1) {
+                System.out.println("+");
+            } else {
+                System.out.println(0);
+            }
         }
     }
 }
